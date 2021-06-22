10ddb6d1bf475079caea35337eb954a90b6d2c7d
captain-tab <d491405879@gmail.com>
2021-06-22 16:34:52 +0800

    build up my space station

---
+++ src/components/TestUndo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>space traveling</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TestUndo extends Vue {}
+</script>
+
+<style scoped></style>
---
+++ src/views/Space.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <test-undo />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import TestUndo from "@/components/TestUndo.vue";
+
+@Component({
+  components: {
+    TestUndo,
+  },
+})
+export default class Space extends Vue {}
+</script>
